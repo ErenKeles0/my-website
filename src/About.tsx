c07e578cb6f9a9d23c73c18f80f87063086f602a
Eren Keleş <09erenkeles09@gmail.com>
2025-09-17 03:41:27 +0300

    Hi Component Created

---
+++ src/About.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
+import BlurText from "./BlurText";
+import './About.css';
+
+export default function About() {
+  const [showSecond, setShowSecond] = useState(false);
+
+  useEffect(() => {
+    gsap.from(".ppdiv", {
+      x: 100,
+      opacity: 0,
+      duration: 1,
+      ease: "power2.out",
+      delay: 1
+    });
+  }, []); // <-- boş array: sadece mountta çalışır
+
+  return (
+    <div className="AboutContainer">
+      <div className='ppdiv'>
+        <img src="PP.png" className="profile-picture" alt="PP" />
+      </div>
+      <div className="texts-container">
+        <div className="HiText1c">        
+        <BlurText
+          text="Hi!"
+          delay={800}
+          animateBy="words"
+          direction="bottom"
+          onAnimationComplete={() => setShowSecond(true)}
+          className="HiText1 montserrat-bold"
+        /></div>
+
+        <div className="HiText2c">{showSecond && (
+          <BlurText
+            text="Welcome to my portfolio."
+            delay={200}
+            animateBy="words"
+            direction="bottom"
+            className="HiText2 montserrat-bold"
+          />
+        )}</div>
+        
+      </div>
+    </div>
+  )
+}
--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 import Card from './Card'
 import { useEffect, useState } from "react";
 import Container from './Container';
-
-
+import About from './About';
+import LightRays from './LightRays.tsx';
 export default function App() {
     type Item = {
       link: string;
@@ -22,7 +22,21 @@ export default function App() {
   
   return (
     <>
-
+      <div className='Rayscontainer' >
+        <LightRays
+            raysOrigin="top-center"
+            raysColor="#adb5bd"
+            raysSpeed={0.5}
+            lightSpread={0.8}
+            rayLength={1.8}
+            followMouse={true}
+            mouseInfluence={0.1}
+            noiseAmount={0.1}
+            distortion={0.05}
+            className="custom-rays"
+        />
+      </div>
+    <About />
     <Container>
       {items.map((item, index) => (
         <Card
